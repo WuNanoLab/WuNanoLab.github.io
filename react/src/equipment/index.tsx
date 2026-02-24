ac177a18ade80874604082a1c313a7888a2d1bd1
vs <1043064987@qq.com>
2026-02-23 22:22:07 -0600

    gallery

--- react/src/equipment/index.tsx
+++ react/src/equipment/index.tsx
@@ -4,11 +4,12 @@ import {
   FluentProvider, webLightTheme,
 } from "@fluentui/react-components";
 import '../index.css'
+import { EquipmentGallery } from './gallery';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FluentProvider theme={webLightTheme}>
-      
+      <EquipmentGallery data={} />
     </FluentProvider>
   </StrictMode>,
 )
